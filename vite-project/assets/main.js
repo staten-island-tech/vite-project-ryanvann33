3abdecae9c4923da72210a0b15d6dff134cd07d5
ryanvann33 <rvann377@gmail.com>
2022-12-20 14:01:20 -0500

    wdawd

---
+++ vite-project/assets/main.js
@@ -0,0 +1,19 @@
+const data = 
+      {
+        name: "Ice Cream",
+        price: "6.50",
+        menu_number: "1"
+
+        id: 663712,
+        original_language: "en",
+        original_title: "Terrifier 2",
+        overview:
+          "After being resurrected by a sinister entity, Art the Clown returns to Miles County where he must hunt down and destroy a teenage girl and her younger brother on Halloween night.  As the body count rises, the siblings fight to stay alive while uncovering the true nature of Art's evil intent.",
+        popularity: 7117.087,
+        poster_path: "/wRKHUqYGrp3PO91mZVQ18xlwYzW.jpg",
+        release_date: "2022-10-06",
+        title: "Terrifier 2",
+        video: false,
+        vote_average: 7.2,
+        vote_count: 305,
+      }
