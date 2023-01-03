7690c0fa3299ef7782641a7f8cfb7f08a75153c8
ryanvann33 <rvann377@gmail.com>
2023-01-03 14:01:08 -0500

    peepeepoopoo

--- vite-project/js/array.js
+++ vite-project/js/array.js
@@ -4,28 +4,67 @@ const menu = [
     price: "6.50",
     menu_number: "1",
     type: "food",
+    img: "https://i.pinimg.com/736x/ae/dc/16/aedc167dc4a404f9396ea9b6a6e8ddf1.jpg",
   },
   {
     name: "fudge-brownie",
     price: "4.00",
     menu_number: "2",
     type: "food",
+    img: "https://dishingouthealth.com/wp-content/uploads/2022/01/HealthyBrownies_Square.jpg",
   },
   {
     name: "milkshake",
     price: "5.30",
     menu_number: "3",
     type: "drink",
+    img: "https://breadsandsweets.com/wp-content/uploads/2022/07/chocolate-milkshake-sq-1-of-1.jpg",
   },
-  { name: "lavacake", price: "9.00", menu_number: "4", type: "food", },
+  {
+    name: "lavacake",
+    price: "9.00",
+    menu_number: "4",
+    type: "food",
+    img: "https://www.cookinwithmima.com/wp-content/uploads/2018/09/molten-chocolate-lava-cake.jpg",
+  },
 
-  { name: "cinnamon-roll-bread", price: "8.00", menu_number: "5", type: "food", },
+  {
+    name: "cinnamon-roll-bread",
+    price: "8.00",
+    menu_number: "5",
+    type: "food",
+    img: "https://media.30seconds.com/tip/lg/Easy-Cinnamon-Roll-Bread-Recipe-This-Cinnamon-Roll-Loaf-Re-21440-1ae9fdf90c-1625928431.jpg",
+  },
 
-  { name: "malted", price: "7.50", menu_number: "6", type: "drink",  },
+  {
+    name: "malted",
+    price: "7.50",
+    menu_number: "6",
+    type: "drink",
+    img: "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/f96d7695-f24d-4a23-9501-f905b6553211/Derivates/95630b7c-f682-435a-85e7-c1d22048a22c.jpg",
+  },
 
-  { name: "smores-cookie", price: "5.00", menu_number: "7", type: "food", },
+  {
+    name: "smores-cookie",
+    price: "5.00",
+    menu_number: "7",
+    type: "food",
+    img: "https://cookiesandcups.com/wp-content/uploads/2021/03/smorescookies.jpg",
+  },
 
-  { name: "coffee-cake", price: "5.40", menu_number: "8", type: "food", },
-  { name: "oreo-cake", price: "8.00", menu_number: "9", type: "food", },
+  {
+    name: "coffee-cake",
+    price: "5.40",
+    menu_number: "8",
+    type: "food",
+    img: "https://www.thechunkychef.com/wp-content/uploads/2020/07/coffee-cake-recipe-card.jpg",
+  },
+  {
+    name: "oreo-cake",
+    price: "8.00",
+    menu_number: "9",
+    type: "food",
+    img: "https://www.lifeloveandsugar.com/wp-content/uploads/2015/02/Chocolate-Oreo-Cake2.jpg",
+  },
 ];
- export let Menu = menu;
+export let Menu = menu;
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -8,113 +8,132 @@ const DOM = {
   food: document.getElementById("food"),
   drink: document.getElementById("drink"),
   price: document.getElementById("price"),
+  button: document.getElementById("btn"),
 };
 
-document.querySelector(".btn").addEventListener("click", function(){
+DOM.button.addEventListener("click", function () {
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  } else {
     document.body.classList.add("dark");
-})
+    document.body.classList.remove("light");
+  }
+});
 
-
-
-function buildMain() {
-  Menu.forEach((food) => {
-    DOM.main.insertAdjacentHTML(
+function makecard() {
+  document.getElementById("dessert").innerHTML = "";
+  Menu.filter((dessert) => food.img).forEach((dessert) => {
+    document.getElementById("dessert").insertAdjacentHTML(
       "beforeend",
-      `<div class="box">
-      <img class="img" src="${food.img}"></img>
-    <p id="text">Food: ${food.name}</p>
-    <p id="text">Meal Type: ${food.meal}</p>
-    <p id="text">Price: $${food.price}</p>
-   </div>`
+      `<div class="flexlr" id=${dessert.name}>
+      <div id= "card"><img class= "" src=""/>
+      <p></p>
+      </div>    
+      </div>`
     );
   });
 }
-buildMain();
 
-let alreadyFilteredbyBreakfast = false;
-DOM.breakfast.addEventListener("click", function () {
-  DOM.main.innerHTML = "";
-  if (!alreadyFilteredbyBreakfast) {
-    Menu.filter((food) => food.meal.includes("Breakfast")).forEach((food) => {
-      DOM.main.insertAdjacentHTML(
-        "beforeend",
-        `<div class="box">
-    <img class="img" src="${food.img}"></img>
-  <p id="text">Food: ${food.name}</p>
-  <p id="text">Meal Type: ${food.meal}</p>
-  <p id="text">Price: $${food.price}</p>
- </div>`
-      );
-      alreadyFilteredbyBreakfast = true;
-    });
-  } else {
-    buildMain();
-    alreadyFilteredbyBreakfast = false;
-  }
-});
+// function buildMain() {
+//   Menu.forEach((food) => {
+//     DOM.main.insertAdjacentHTML(
+//       "beforeend",
+//       `<div class="box">
+//       <img class="img" src="${food.img}"></img>
+//     <p id="text">Food: ${food.name}</p>
+//     <p id="text">Meal Type: ${food.meal}</p>
+//     <p id="text">Price: $${food.price}</p>
+//    </div>`
+//     );
+//   });
+// }
+// buildMain();
 
-let alreadyFilteredbyLunch = false;
-DOM.lunch.addEventListener("click", function () {
-  DOM.main.innerHTML = "";
-  if (!alreadyFilteredbyLunch) {
-    Menu.filter((food) => food.meal.includes("Lunch")).forEach((food) => {
-      DOM.main.insertAdjacentHTML(
-        "beforeend",
-        `<div class="box">
-    <img class="img" src="${food.img}"></img>
-  <p id="text">Food: ${food.name}</p>
-  <p id="text">Meal Type: ${food.meal}</p>
-  <p id="text">Price: $${food.price}</p>
- </div>`
-      );
-      alreadyFilteredbyLunch = true;
-    });
-  } else {
-    buildMain();
-    alreadyFilteredbyLunch = false;
-  }
-});
+// let alreadyFilteredbyBreakfast = false;
+// DOM.breakfast.addEventListener("click", function () {
+//   DOM.main.innerHTML = "";
+//   if (!alreadyFilteredbyBreakfast) {
+//     Menu.filter((food) => food.meal.includes("Breakfast")).forEach((food) => {
+//       DOM.main.insertAdjacentHTML(
+//         "beforeend",
+//         `<div class="box">
+//     <img class="img" src="${food.img}"></img>
+//   <p id="text">Food: ${food.name}</p>
+//   <p id="text">Meal Type: ${food.meal}</p>
+//   <p id="text">Price: $${food.price}</p>
+//  </div>`
+//       );
+//       alreadyFilteredbyBreakfast = true;
+//     });
+//   } else {
+//     buildMain();
+//     alreadyFilteredbyBreakfast = false;
+//   }
+// });
 
-let alreadyFilteredbyDinner = false;
-DOM.dinner.addEventListener("click", function () {
-  DOM.main.innerHTML = "";
-  if (!alreadyFilteredbyDinner) {
-    Menu.filter((food) => food.meal.includes("Dinner")).forEach((food) => {
-      DOM.main.insertAdjacentHTML(
-        "beforeend",
-        `<div class="box">
-    <img class="img" src="${food.img}"></img>
-  <p id="text">Food: ${food.name}</p>
-  <p id="text">Meal Type: ${food.meal}</p>
-  <p id="text">Price: $${food.price}</p>
- </div>`
-      );
-      alreadyFilteredbyDinner = true;
-    });
-  } else {
-    buildMain();
-    alreadyFilteredbyDinner = false;
-  }
-});
+// let alreadyFilteredbyLunch = false;
+// DOM.lunch.addEventListener("click", function () {
+//   DOM.main.innerHTML = "";
+//   if (!alreadyFilteredbyLunch) {
+//     Menu.filter((food) => food.meal.includes("Lunch")).forEach((food) => {
+//       DOM.main.insertAdjacentHTML(
+//         "beforeend",
+//         `<div class="box">
+//     <img class="img" src="${food.img}"></img>
+//   <p id="text">Food: ${food.name}</p>
+//   <p id="text">Meal Type: ${food.meal}</p>
+//   <p id="text">Price: $${food.price}</p>
+//  </div>`
+//       );
+//       alreadyFilteredbyLunch = true;
+//     });
+//   } else {
+//     buildMain();
+//     alreadyFilteredbyLunch = false;
+//   }
+// });
 
-let alreadyFilteredbyPrice = false;
-DOM.price.addEventListener("click", function () {
-  DOM.main.innerHTML = "";
-  if (!alreadyFilteredbyPrice) {
-    Menu.filter((food) => food.price < 5).forEach((food) => {
-      DOM.main.insertAdjacentHTML(
-        "beforeend",
-        `<div class="box">
-    <img class="img" src="${food.img}"></img>
-  <p id="text">Food: ${food.name}</p>
-  <p id="text">Meal Type: ${food.meal}</p>
-  <p id="text">Price: $${food.price}</p>
- </div>`
-      );
-      alreadyFilteredbyPrice = true;
-    });
-  } else {
-    buildMain();
-    alreadyFilteredbyPrice = false;
-  }
-});
+// let alreadyFilteredbyDinner = false;
+// DOM.dinner.addEventListener("click", function () {
+//   DOM.main.innerHTML = "";
+//   if (!alreadyFilteredbyDinner) {
+//     Menu.filter((food) => food.meal.includes("Dinner")).forEach((food) => {
+//       DOM.main.insertAdjacentHTML(
+//         "beforeend",
+//         `<div class="box">
+//     <img class="img" src="${food.img}"></img>
+//   <p id="text">Food: ${food.name}</p>
+//   <p id="text">Meal Type: ${food.meal}</p>
+//   <p id="text">Price: $${food.price}</p>
+//  </div>`
+//       );
+//       alreadyFilteredbyDinner = true;
+//     });
+//   } else {
+//     buildMain();
+//     alreadyFilteredbyDinner = false;
+//   }
+// });
+
+// let alreadyFilteredbyPrice = false;
+// DOM.price.addEventListener("click", function () {
+//   DOM.main.innerHTML = "";
+//   if (!alreadyFilteredbyPrice) {
+//     Menu.filter((food) => food.price < 5).forEach((food) => {
+//       DOM.main.insertAdjacentHTML(
+//         "beforeend",
+//         `<div class="box">
+//     <img class="img" src="${food.img}"></img>
+//   <p id="text">Food: ${food.name}</p>
+//   <p id="text">Meal Type: ${food.meal}</p>
+//   <p id="text">Price: $${food.price}</p>
+//  </div>`
+//       );
+//       alreadyFilteredbyPrice = true;
+//     });
+//   } else {
+//     buildMain();
+//     alreadyFilteredbyPrice = false;
+//   }
+// });
